6821b9f740e2a72775158ac08e6933095c9b4d61
Arvin Singh <arvinsingh@gmail.com>
2018-10-14 01:49:35 +0530

    FYP material.

---
+++ src/feature_generators/TargetFeatureGenerator.py
@@ -0,0 +1,37 @@
+from FeatureGenerator import *
+import pickle
+import pandas as pd
+from helpers import *
+
+
+class TargetFeatureGenerator(FeatureGenerator):
+    
+    '''
+        doing nothing other than returning the target variables
+    '''
+
+    def __init__(self, name='targetFeatureGenerator'):
+        super(TargetFeatureGenerator, self).__init__(name)
+
+
+    def process(self, df, header='train'):
+
+        targets = df['target'].values
+        outfilename_target = "%s.target.pkl" % header
+        with open("saved_data/" + outfilename_target, "wb") as outfile:
+            pickle.dump(targets, outfile, -1)
+        print ('targets saved in %s' % outfilename_target)
+        
+        return targets
+
+
+    def read(self, header='train'):
+
+        filename_target = "%s.target.pkl" % header
+        with open("saved_data/" + filename_target, "rb") as infile:
+            target = pickle.load(infile)
+            print ('target.shape:')
+            print (target.shape)
+
+        return target
+
